41fc62e9428fe292c13ce7f1cf501348a744bd59
Igor Decess <i.decess@comcast.net>
2022-05-12 15:13:49 -0600

    Add movie title and poster to movie card file

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './MovieCard.css';
 
-const MovieCard = () => {
-
+const MovieCard = (props) => {
+  return (
+    <div className='movie-card'>
+      <img src={props.poster} />
+      <h2>{props.title}</h2>
+    </div>
+  )
 }
 
 
