3185c30c282ba043dac1d98242f8e1e9f023e6be
Igor Decess <i.decess@comcast.net>
2022-05-12 15:06:32 -0600

    Add movie card to movie container

--- src/MovieContainer.js
+++ src/MovieContainer.js
@@ -1,11 +1,25 @@
 import React from 'react';
 // import App from './App';
 import './MovieContainer.css';
+import MovieCard from './MovieCard';
 
-const MovieContainer = () => {
+const MovieContainer = (props) => {
+  const movieCards = props.movies.map(movie => {
+    return (
+      <MovieCard
+        id={movie.id}
+        key={movie.id}
+        poster={movie.poster_path}
+        title={movie.title}
+      />
+    )
+  })
 
-
-
+  return (
+    <div className='movie-container'>
+      {movieCards}
+    </div>
+  )
 }
 
 
