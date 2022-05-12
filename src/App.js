aba074fe917c6aebd67325cac0f5e499fd1083c0
Igor Decess <i.decess@comcast.net>
2022-05-12 14:32:54 -0600

    Add state, add componentDidMount method, and add rendering MovieContainer in App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import movieData from './data';
 // import logo from './logo.svg';
 import './App.css';
+import MovieContainer from './MovieContainer';
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      movies: []
+
+    }
+  }
+  componentDidMount = () => {
+    this.setState({movies: movieData.movies})
+    console.log(movieData.movies)
   }
 
 
@@ -13,6 +22,7 @@ class App extends Component {
     return (
       <main className='App'>
         <h1>Rancid Tomatillos</h1>
+        <MovieContainer movies={this.state.movies}/>
       </main>
     )
   }
---
+++ src/MovieContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import App from './App';
+import './MovieContainer.css';
+
+const MovieContainer = () => {
+
+
+
+}
+
+
+
+export default MovieContainer;
