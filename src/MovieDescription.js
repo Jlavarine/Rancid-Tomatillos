21f0b8c3af46bb278ec9b56530cb9c91772568aa
Igor Decess <i.decess@comcast.net>
2022-05-14 11:04:05 -0600

    Add and fix error handling for MovieDescription.js

--- src/MovieDescription.js
+++ src/MovieDescription.js
@@ -31,21 +31,27 @@ class MovieDescription extends Component {
   }
 
   render() {
-    return (
-      <div style={{backgroundImage: `url(${this.state.movie.backdrop_path})`}} className='movie-description' src={this.state.movie.backdrop_path}>
-        <div className='poster-box'>
-          <img className='poster-path' src={this.state.movie.poster_path}/>
+    if (!this.state.error) {
+      return (
+        <div style={{backgroundImage: `url(${this.state.movie.backdrop_path})`}} className='movie-description' src={this.state.movie.backdrop_path}>
+          <div className='poster-box'>
+            <img className='poster-path' src={this.state.movie.poster_path}/>
+          </div>
+          <div className='movie-details-box'>
+            <p>Title: {this.state.movie.title}</p>
+            <p>Overview: {this.state.movie.overview}</p>
+            <p>Runtime: {this.state.movie.runtime} mins</p>
+            <p>Average Rating: {this.state.movie.average_rating}</p>
+            <p>Release Date: {this.state.movie.release_date}</p>
+            <button onClick={() => this.props.returnToMainView()}>Return</button>
+          </div>
         </div>
-        <div className='movie-details-box'>
-          <p>Title: {this.state.movie.title}</p>
-          <p>Overview: {this.state.movie.overview}</p>
-          <p>Runtime: {this.state.movie.runtime} mins</p>
-          <p>Average Rating: {this.state.movie.average_rating}</p>
-          <p>Release Date: {this.state.movie.release_date}</p>
-          <button onClick={() => this.props.returnToMainView()}>Return</button>
-        </div>
-      </div>
-    )
+      )
+    } else if (this.state.error) {
+      return (
+        <h1 className='App'>{this.state.error}</h1>
+      )
+    }
   }
 }
 
