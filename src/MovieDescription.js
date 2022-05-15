63273c997af8fbff555b9bcbaf47e465b8314976
Jacob Lavarine <Jlavarine6237@gmail.com>
2022-05-15 13:50:03 -0500

    Change title and refactor fetch functions

--- src/MovieDescription.js
+++ src/MovieDescription.js
@@ -7,23 +7,38 @@ class MovieDescription extends Component {
     super(props)
     this.state = {
       movie: '',
+      videos: '',
       error: ''
     }
   }
 
   componentDidMount = () => {
     fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.movies.id}`)
-    // .then(response => response.json())
     .then(response => {
-      // if (response.status === '500') {
       if (!response.ok) {
         this.setState({error: 'Something went wrong, please refresh!'})
       } else {
-        // console.log(response)
         return response.json()
       }
     })
     .then(data => this.setState({movie: data.movie}))
+    .catch(error => {
+      console.log('Something went wrong, please refresh!')
+      this.setState({error: 'Something went wrong, please refresh!'})
+    })
+    this.fetchTrailerData()
+  }
+
+  fetchTrailerData = () => {
+    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.movies.id}/videos`)
+    .then(response => {
+      if (!response.ok) {
+        this.setState({error: 'Something went wrong, please refresh!'})
+      } else {
+        return response.json()
+      }
+    })
+    .then(data => this.setState({videos: data.videos[0]}))
     .catch(error => {
       console.log('Something went wrong, please refresh!')
       this.setState({error: 'Something went wrong, please refresh!'})
@@ -37,7 +52,11 @@ class MovieDescription extends Component {
           <div className='poster-box'>
             <img className='poster-path' src={this.state.movie.poster_path}/>
           </div>
-          <div className='movie-details-box'>
+          <div className='trailer-details-box'>
+            <div className='trailer-box'>
+            <iframe src={`http://www.youtube.com/embed/${this.state.videos.key}`} width='900px' height='400px'></iframe>
+            </div>
+            <div className='movie-details-box'>
             <p>Title: {this.state.movie.title}</p>
             <br/>
             <p>Overview: {this.state.movie.overview}</p>
@@ -49,6 +68,7 @@ class MovieDescription extends Component {
             <p>Release Date: {this.state.movie.release_date}</p>
             <br/>
             <button onClick={() => this.props.returnToMainView()}>Return</button>
+            </div>
           </div>
         </div>
       )
