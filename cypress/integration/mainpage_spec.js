bfeda61d076d3b46e4c4092f68ea2400c0f1febf
Igor Decess <i.decess@comcast.net>
2022-05-16 18:36:30 -0600

    Add cypress file and E2E testing for the main page

---
+++ cypress/integration/mainpage_spec.js
@@ -0,0 +1,36 @@
+describe('Rancid Tomatillos main page', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('should be able to visit the page and render the correct elements', () => {
+    cy.contains('🍿 Rancid Tomatillos 🍿')
+    .get('.movie-container')
+    .children()
+    .eq(1)
+    .contains('Mulan');
+  });
+
+  it('should be able to fetch movie data and display it on the page', () => {
+    cy.intercept({
+      method: 'GET',
+      url: 'https://rancid-tomatillos.herokuapp.com/api/v2/movies'
+    },
+    []
+  ).as('getMovies');
+    // cy.get('getMovies')
+    //   .should('have.length', 40)
+  });
+
+  it('should not be able to fetch movie data and should display error message', () => {
+    cy.intercept({
+      method: 'GET',
+      url: 'https://rancid-tomatillos.herokuapp.com/api/v2/movies',
+    }, {
+      forceNetworkError: true
+    }),
+    // cy.visit('http://localhost:3000')
+    cy.contains('h1', 'Something went wrong, please refresh!');
+  });
+});
