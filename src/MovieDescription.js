a20c61477e06d6c353a16a7662e47d2d79c77c92
Igor Decess <i.decess@comcast.net>
2022-05-17 17:14:33 -0600

    Add router's Return button functionality

--- src/MovieDescription.js
+++ src/MovieDescription.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import App from './App'
 import './MovieDescription.css'
+import { Link } from 'react-router-dom'
 
 class MovieDescription extends Component {
   constructor(props) {
@@ -46,32 +47,33 @@ class MovieDescription extends Component {
   }
 
   render() {
-      return (
-            <div style={{backgroundImage: `url(${this.state.movie.backdrop_path})`}} className='movie-description' src={this.state.movie.backdrop_path}>
-              <div className='poster-box'>
-                <img className='poster-path' src={this.state.movie.poster_path}/>
-              </div>
-              <div className='trailer-details-box'>
-                <div className='trailer-box'>
-                <iframe src={`http://www.youtube.com/embed/${this.state.videos.key}`} width='900px' height='400px'></iframe>
-                </div>
-                <div className='movie-details-box'>
-                <p>Title: {this.state.movie.title}</p>
-                <br/>
-                <p>Overview: {this.state.movie.overview}</p>
-                <br/>
-                <p>Runtime: {this.state.movie.runtime} mins</p>
-                <br/>
-                <p>Average Rating: {this.state.movie.average_rating}</p>
-                <br/>
-                <p>Release Date: {this.state.movie.release_date}</p>
-                <br/>
-                <button className='return-button' onClick={() => this.props.returnToMainView()}>Return</button>
-                </div>
-              </div>
-            </div>
-      )
-}
+    return (
+      <div style={{backgroundImage: `url(${this.state.movie.backdrop_path})`}} className='movie-description' src={this.state.movie.backdrop_path}>
+        <div className='poster-box'>
+          <img className='poster-path' src={this.state.movie.poster_path}/>
+        </div>
+        <div className='trailer-details-box'>
+          <div className='trailer-box'>
+          <iframe src={`http://www.youtube.com/embed/${this.state.videos.key}`} width='900px' height='400px'></iframe>
+          </div>
+          <div className='movie-details-box'>
+          <p>Title: {this.state.movie.title}</p>
+          <br/>
+          <p>Overview: {this.state.movie.overview}</p>
+          <br/>
+          <p>Runtime: {this.state.movie.runtime} mins</p>
+          <br/>
+          <p>Average Rating: {this.state.movie.average_rating}</p>
+          <br/>
+          <p>Release Date: {this.state.movie.release_date}</p>
+          <br/>
+          <Link to='/'><button className='return-button' onClick={() => this.props.returnToMainView()}>Return</button>
+          </Link>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default MovieDescription
